5b663c2ae0e372fb0e3e35cb96a3e7113d268428
azizahra11 <razizahnur6@gmail.com>
2024-06-28 07:06:25 +0000

    azizah

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,13 +37,13 @@ if check_btn :
 radio_button= st.radio('Choose below',[x for x in range(1,3)])
 st.write('Anda Memilih',radio_button)
     
-  #slider 
-  age_slider = st.slider('Berapa Usia Anda',0,100)
-  st.write('Usia Anda',age_slider)
+#slider 
+age_slider = st.slider('Berapa Usia Anda',0,100)
+st.write('Usia Anda',age_slider)
     
-  #Input (Typing)
-  num_input = st.number_input('Input Berapapun')
-  st.write('Kuadrat dari {} adalah {}'.format(num_input,num_input**2))
+#Input (Typing)
+num_input = st.number_input('Input Berapapun')
+st.write('Kuadrat dari {} adalah {}'.format(num_input,num_input**2))
       
 if __name__ == '__main__' : 
   main()
