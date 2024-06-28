c1ec7ea3a6ffdf4006605f42c5e971b41d999762
azizahra11 <razizahnur6@gmail.com>
2024-06-28 06:57:53 +0000

    azizah

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,34 @@ def main() :
 
     st.dataframe(house)
 
+# Untuk menulis metric
     st.write('Metrics')
     st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
 if __name__ == '__main__' : 
     main()
+
+ # Untuk menampilkan Grid -- masih blm tampil
+    #st.write('Menampilkan Dataframe dengan St AgGrid')
+    #AgGrid(house)
+
+st.table([x for x in range(1,5)])
+  
+  click_me_btn = st.button('Click Me')
+  st.write(click_me_btn) #Return True kalo di Click 
+  check_btn = st.checkbox('Klik Jika Setuju')
+  if check_btn :
+      st.write('Anda Setuju')
+  
+  radio_button= st.radio('Choose below',[x for x in range(1,3)])
+  st.write('Anda Memilih',radio_button)
+    
+  #slider 
+  age_slider = st.slider('Berapa Usia Anda',0,100)
+  st.write('Usia Anda',age_slider)
+    
+  #Input (Typing)
+  num_input = st.number_input('Input Berapapun')
+  st.write('Kuadrat dari {} adalah {}'.format(num_input,num_input**2))
+      
+if __name__ == '__main__' : 
+  main()
