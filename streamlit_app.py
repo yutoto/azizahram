d8b0597c325660053ec9b0fc40fa3964642251b0
azizahra11 <razizahnur6@gmail.com>
2024-06-28 03:55:55 +0000

    azizah

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
 def main() :
-    st.write('This is Header')
+    st.write('Halaman Streamlit Azizah')
     st.subheader('This is Subheader')
     st.markdown('# Rendering Markdown ')
-    st.write('Some Pyhtagorean Equation_apaaja : ')
+    st.write('Some Pyhtagorean Equation : ')
     st.latex('c^2 = a^2+b^2')
 
 if __name__ == '__main__' :
