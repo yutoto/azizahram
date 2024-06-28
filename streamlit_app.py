ecda8896ff022a35bf75b536372be2b11ecbab9e
Adi <undip.nugroho@gmail.com>
2024-06-28 15:40:20 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,8 +19,8 @@ def main() :
 # Untuk menulis metric
     st.write('Metrics')
     st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
-#if __name__ == '__main__' : 
-    #main()
+if __name__ == '__main__' : 
+    main()
 
 # Untuk menampilkan Grid -- masih blm tampil
 st.write('Menampilkan Dataframe dengan St AgGrid')
@@ -114,5 +114,5 @@ with col3:
 #else:
     #st.write("Tidak ada konten untuk ditampilkan.")
   
-if __name__ == '__main__' :
-    main()
+#if __name__ == '__main__' :
+    #main()
