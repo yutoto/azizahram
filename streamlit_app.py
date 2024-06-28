3bc5c7b032bb5e5e85948178780c1357fadf27ff
azizahra11 <razizahnur6@gmail.com>
2024-06-28 03:13:40 +0000

    apaaja

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,8 @@ def main() :
     st.write('This is Header')
     st.subheader('This is Subheader')
     st.markdown('# Rendering Markdown ')
-    st.write('Some Pyhtagorean Equation : ')
-    st.later('c^2 = a^2+b^2')
+    st.write('Some Pyhtagorean Equation_apaaja : ')
+    st.latex('c^2 = a^2+b^2')
 
 if __name__ == '__main__' :
     main()
